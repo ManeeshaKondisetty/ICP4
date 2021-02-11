c0c883501132390ab72fb54bd69057f6e5045004
ManeeshaKondisetty <mpk5fd@umsystem.edu>
2021-02-10 20:12:11 -0600

    updated

--- GitHubUserFinder/script.js
+++ GitHubUserFinder/script.js
@@ -1,39 +1,48 @@
 function getGithubInfo(user) {
-    //1. Create an instance of XMLHttpRequest class and send a GET request using it.
+    //1. Create an instance of Ajax request and send a URL using it.
     // The function should finally return the object(it now contains the Response!)
-    const xhReq = new XMLHttpRequest();
-
-    const reqURL = `https://api.github.com/users/${user}`;
-    //sending API request
-    xhReq.open('GET', reqURL, false);
-
-    xhReq.onload = function () {
-        return this;
-    }
-    //returning the response to the parent function
-    xhReq.send();
-    return xhReq.onload();
+    $.ajax({
+        url: "https://api.github.com/users/" + user,
+        type: 'GET',
+        success: function (response, status, statusCode) {
+            if (statusCode.status === 200) {
+                showUser(response);
+            } else {
+                noSuchUser(user);
+            }
+        },
+        error: function (response, status, statusCode) {
+            noSuchUser(user);
+        }
+    })
 }
 
 function showUser(user) {
     //2. set the contents of the h2 and the two div elements in the div '#profile' with the user content
     //setting visibility condition of the user details table
+    document.getElementById("noUser").style.visibility = "hidden";
     document.getElementById("profile").style.visibility = "visible";
     //hiding the Invalid user message
-    document.getElementById("noUser").style.visibility = "hidden";
     //display the username of the user
-    document.getElementById("heading").innerHTML = user.login;
+    $("#heading").text(user.login);
     //setting ID to display user id
-    document.getElementById("userid").innerHTML = user.id;
+    $("#userid").text(user.id);
     //display profile pic
-    document.getElementById("proPic").src = user.avatar_url;
+    $("#proPic").attr("src", user.avatar_url);
+
+    $('#proPic').click(function () {
+        window.open($(this)[0].src, '_blank')
+    });
+
     //display the number of public repos
-    document.getElementById("repos").innerHTML = user.public_repos;
+    $('#repos').text(user.public_repos);
     //Providing the link to user's profile
-    document.getElementById("proLink").innerText = "Link to profile";
-    document.getElementById("proLink").href = user.html_url;
-    document.getElementById("name").innerHTML = user.name;
+    const proLink = $("#proLink");
+    proLink.text("Link to Profile");
+    proLink.attr("href", user.html_url);
+    $('#name').text(user.name);
 }
+
 //invalid username or no such user function
 function noSuchUser(username) {
     //3. set the elements such that a suitable message is displayed
@@ -54,18 +63,8 @@ $(document).ready(function () {
             let username = $(this).val();
             //reset the text typed in the input
             $(this).val("");
-            //get the user's information and store the Respsonse
-            const obj = getGithubInfo(username);
-            const response = obj.response;
-            const statusCode = obj.status;
-            //if the Response is successful show the user's details
-
-            if (statusCode === 200) {
-
-                showUser(JSON.parse(response));
-            } else {
-                noSuchUser(username);
-            }
+            //get the user's information and store the respsonse
+            getGithubInfo(username);
         }
     })
-});
+})
